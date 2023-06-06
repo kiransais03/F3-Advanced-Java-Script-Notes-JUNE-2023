58702715b4c38a05b605ea018229b5a3fa00c6a9
Prikshit <prikshit.chawla@walmart.com>
2023-06-06 22:41:26 +0530

    CALL & APPLY

---
+++ callBindApply.js
@@ -0,0 +1,47 @@
+/// Call bind & Apply are 3 important
+// methods available to all JS function
+// which are used to set THIS keyword
+// inside functions. independent of how the
+// function is called.
+
+// Call
+// let obj = {
+//     name: 'PC',
+// };
+
+// function sayHello(age, company){
+//     return "Hello " + this.name + " is " + age + " " + company;
+// }
+
+// // console.log(sayHello());
+// console.log(sayHello.call(obj,23, 'walmart'));
+
+// /// call -> obj, arguments
+
+// Apply
+let obj = {
+    name: 'PC',
+};
+
+function sayHello(age, company){
+    return "Hello " + this.name + " is " + age + " " + company;
+}
+
+console.log(sayHello.call(obj,23, 'walmart'));
+console.log(sayHello(23,'walmart'));
+
+///Bind
+
+// let obj = {
+//     name: 'PC',
+// };
+
+// function sayHello(age, company){
+//     return 'Hello '+ this.name + ' is ' + age + ' ' +company;
+// }
+
+// const hello=sayHello.bind(obj); // this will give me a function with sayHello
+// // defi.
+
+// console.log(hello());
+
