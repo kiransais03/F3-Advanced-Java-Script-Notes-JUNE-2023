cc7b190cf38167e70686cf36f2259ec39d98b5ff
Prikshit <prikshit.chawla@walmart.com>
2023-06-12 22:01:37 +0530

    reduce

--- ArrayPolyfills.js
+++ ArrayPolyfills.js
@@ -1,46 +1,79 @@
 
 
-// Array.prototype.myMap = function(callBack){
+// // Array.prototype.myMap = function(callBack){
+// //     let temp=[];
+// //     for (let i = 0; i < this.length; i++) {
+// //       temp.push(callBack(this[i], i));
+// //     }
+// //     return temp;
+// // }
+
+// // const mutliplyByThree = nums.myMap((currElement, index) => {
+// //     return 9;
+// // });
+
+// // const mutliplyByTwo = nums.myMap((currElement) => {
+// //     return currElement*2;
+// // });
+
+
+// // console.log(nums);
+// // console.log(mutliplyByThree);
+// // console.log(mutliplyByTwo);
+// const nums = [1,2,3,4];
+
+
+// Array.prototype.myFilter = function(callBack){
 //     let temp=[];
 //     for (let i = 0; i < this.length; i++) {
-//       temp.push(callBack(this[i], i));
+//       if (callBack(this[i], i)) {
+//         temp.push(this[i]);
+//       }
 //     }
 //     return temp;
 // }
 
-// const mutliplyByThree = nums.myMap((currElement, index) => {
-//     return 9;
+// // filter -> 
+// const moreThanTwo = nums.myFilter((currElement,index)=>{
+//     return currElement>2;
 // });
-
-// const mutliplyByTwo = nums.myMap((currElement) => {
-//     return currElement*2;
-// });
-
 
 // console.log(nums);
-// console.log(mutliplyByThree);
-// console.log(mutliplyByTwo);
-const nums = [1,2,3,4];
-
-
-Array.prototype.myFilter = function(callBack){
-    let temp=[];
-    for (let i = 0; i < this.length; i++) {
-      if (callBack(this[i], i)) {
-        temp.push(this[i]);
-      }
-    }
-    return temp;
-}
-
-// filter -> 
-const moreThanTwo = nums.myFilter((currElement,index)=>{
-    return currElement>2;
-});
-
-console.log(nums);
-console.log(moreThanTwo);
+// console.log(moreThanTwo);
 
 // reduce polyfills
 // call bind apply polyfills
 // 
+
+// arr.reduce((accumulator,currentElement,index)=>{},initialValue);
+
+// Array.prototype.myReduce = function(callback, initialValue){
+//   // initialValue = value given by user || undefined
+//   var accumulator = initialValue; /// undefined
+//   for(let i=0;i<this.length;i++){
+//     // accumulator = callback(accumulator,this[i],i);
+//     /// if accumulator === undefined -> this[i]
+//     accumulator = accumulator !== undefined ? callback(accumulator, this[i], i) : this[i];
+//   }
+//   return accumulator;
+// }
+
+
+// // const nums = [10001,10,3,4];
+
+// // const sum = nums.myReduce((sumSoFar,currentElement)=>{
+// //     if(currentElement%2===0){
+// //         return sumSoFar+currentElement;
+// //     }
+// //     else return sumSoFar;
+// // }); 
+
+// console.log(sum);
+
+/// multi entity -> one thing
+// array of anything (array of object, number) -> number, string,object
+
+let arr = [1,2,3,4]; // arrays of arrays with nested array depth as 0;
+// [[1,2],3];
+
+// arr[i][j]
